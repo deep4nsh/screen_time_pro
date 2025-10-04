6bd9e790fb1ca3a6217c1fe83569a7510db56b63
Deepansh Gupta <deepansh.gup124@gmail.com>
2025-10-04 16:39:50 +0530

    Unused Yaml files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,13 +1,12 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
     namespace = "com.example.screen_time_pro"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 34  // Fixed: removed duplicate and kept only this
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -20,20 +19,15 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.screen_time_pro"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdk = flutter.minSdkVersion  // Set explicit value
+        targetSdk = 34  // Fixed: use targetSdk not targetSdkVersion
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
